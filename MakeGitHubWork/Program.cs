d2362e645d2dacadb6bf0cfd3b27763f1e50feb6
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-23 15:31:21 +0100

    Add project files.

---
+++ MakeGitHubWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakeGitHubWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
