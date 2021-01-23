b656e212be8096de84fb94dcf9979d355d2072d4
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-23 15:33:20 +0100

    Add changes

--- MakeGitHubWork/Program.cs
+++ MakeGitHubWork/Program.cs
@@ -6,7 +6,7 @@ namespace MakeGitHubWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Add some changes to file...");
         }
     }
 }
